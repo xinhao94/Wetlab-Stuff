6a96d42dad917ef48f9408fc440e05ac5b3a93e0
Xinhao Song <songxinhao@hotmail.com>
2019-05-01 13:20:12 -0500

    Update M9_Calculator.py

--- M9_Calculator.py
+++ M9_Calculator.py
@@ -1,5 +1,7 @@
 # M9 Media Calculator
 
+from prettytable import PrettyTable
+
 def M9_Calculator(M9):
   M9 = float(M9)
   MgSO4 = M9 * 0.002
@@ -7,27 +9,30 @@ def M9_Calculator(M9):
   CaCl2  = M9 * 0.0001
   Casamino_Acid = M9 * 0.01
   
-  print("1x M9 Salt: {} mL".format(M9))
+  table = PrettyTable(['Index', 'Component', 'Volume'])
+  table.add_row(['1', '1x M9 Salt', '{} mL'.format(M9)])
   
   if MgSO4 < 1:
-    print("1M MgSO4: {} μL".format(1000 * MgSO4))
+    table.add_row(['2', '1M MgSO4', '{} μL'.format(1000 * MgSO4)])
   else:
-    print("1M MgSO4: {} mL".format(MgSO4))
+    table.add_row(['2', '1M MgSO4', '{} mL'.format(MgSO4)])
   
   if Glucose < 1:
-    print("20% Glucose: {} μL".format(1000 * Glucose))
+    table.add_row(['3', '20% Glucose', '{} μL'.format(1000 * Glucose)])
   else:
-    print("20% Glucose: {} mL".format(Glucose))
+    table.add_row(['3', '20% Glucose', '{} mL'.format(Glucose)])
     
   if CaCl2 < 1:
-    print("1M CaCl2: {} μL".format(1000 * CaCl2))
+    table.add_row(['4', '1M CaCl2', '{} μL'.format(1000 * CaCl2)])
   else:
-    print("1M CaCl2: {} mL".format(CaCl2))
+    table.add_row(['4', '1M CaCl2', '{} mL'.format(CaCl2)])
     
   if Casamino_Acid < 1:
-    print("10% Casamino Acid: {} μL".format(1000 * Casamino_Acid))
+    table.add_row(['5', 'Casamino Acid', '{} μL'.format(1000 * Casamino_Acid)])
   else:
-    print("10% Casamino Acid: {} mL".format(Casamino_Acid))
+    table.add_row(['5', 'Casamino Acid', '{} mL'.format(Casamino_Acid)])
  
-M9_vol = input('Please enter the volume of 1x M9 salt in mL:')
+  print(table)
+ 
+M9_vol = input('Please enter the volume of 1x M9 salt in mL: ')
 M9_Calculator(M9_vol)
