779f48f18abaa80159ab17864a7f806617c9dcea
Xinhao Song <songxinhao@hotmail.com>
2020-04-19 17:16:53 -0500

    Create Lokta_Solve.m

---
+++ Lokta_Solve.m
@@ -0,0 +1,18 @@
+% Set span 
+t0 = 0; 
+tf = 15;
+% Set initial states
+y0 = [20, 20];
+
+% Obtain numerical solutions
+[t, y] = ode45('Lotka1', [t0,tf], y0);
+
+% Plot the figure
+len = length(y(:,1));
+t = 1:1:len;
+
+close all
+hold on
+plot(t, y(:,1), '-r');
+plot(t, y(:,2), '-g');
+legend('Predator', 'Prey', 'Location', 'NE');
