b6741b445c2271918afef70c721efc71b4c9f67f
Xinhao Song <songxinhao@hotmail.com>
2020-04-19 18:57:11 -0500

    Update Lotka2.m

--- Lotka2.m
+++ Lotka2.m
@@ -5,12 +5,12 @@ dy = zeros(2,1);
 % Set parameters
 r1 = 0.04; 
 r2 = 0.02;
-alpha12 = 5;
-alpha21 = 0;
+alpha21 = 5;
+alpha12 = 0;
 K1 = 500;
 K2 = 500;
 
 % Set function
-dy(1) = r1*y(1)*(1-(y(1)+alpha12*y(2))/K1);
-dy(2) = r2*y(2)*(1-(y(2)+alpha21*y(1))/K2);
+dy(1) = r1*y(1)*(1-(y(1)+alpha21*y(2))/K1);
+dy(2) = r2*y(2)*(1-(y(2)+alpha12*y(1))/K2);
 end
