8991718d65d00346c36b0abd1a88ec0c67a269f4
Xinhao Song <songxinhao@hotmail.com>
2021-03-26 17:09:19 -0500

    Update Growth Curve Fitting.R

--- Growth Curve Fitting.R
+++ Growth Curve Fitting.R
@@ -6,3 +6,7 @@ d <- read.table(sourcePath, header = TRUE, sep = ",", stringsAsFactors = FALSE)
 fitParams <- SummarizeGrowthByPlate(d, plot_fit = TRUE, 
                                     plot_file = "Fit_Curves.pdf")
 fitParams
+
+outputPath <- "Desktop/Fit_Params.csv"
+write.table(fitParams, file = outputPath, 
+            quote = FALSE, sep = ",", row.names = FALSE)
