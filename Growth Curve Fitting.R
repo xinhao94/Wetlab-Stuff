2396565837c5ba660125312b45b62228c98df84e
Xinhao Song <songxinhao@hotmail.com>
2021-06-10 17:53:38 -0500

    Update Growth Curve Fitting.R

--- Growth Curve Fitting.R
+++ Growth Curve Fitting.R
@@ -1,12 +1,12 @@
 library(growthcurver)
 library(Cairo)
 
-sourcePath <- "Desktop/source1.csv"
+sourcePath <- "/Users/xinhao/Desktop/20210608_Source.csv"
 d <- read.table(sourcePath, header = TRUE, sep = ",", stringsAsFactors = FALSE)
 fitParams <- SummarizeGrowthByPlate(d, plot_fit = TRUE, 
-                                    plot_file = "Fit_Curves.pdf")
+                                    plot_file = "20210608_Fit_Curves.pdf")
 fitParams
 
-outputPath <- "Desktop/Fit_Params.csv"
+outputPath <- "/Users/xinhao/Desktop/20210608_Fit_Params.csv"
 write.table(fitParams, file = outputPath, 
             quote = FALSE, sep = ",", row.names = FALSE)
