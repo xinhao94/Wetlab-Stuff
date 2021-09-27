39f0eafd8e214b6b87c5cba65f60877e764781db
Xinhao Song <songxinhao@hotmail.com>
2021-09-27 17:00:31 -0500

    Create AdaptationCurve.py

---
+++ AdaptationCurve.py
@@ -0,0 +1,22 @@
+# E. coli MB1860 Adaptation curve
+import numpy as np
+import matplotlib.pyplot as plt
+
+day = np.arange(0,8)
+conc = [0,0.03,0.0625,0.09,0.18,0.38,1,4]
+MIC = []
+for i in range(8):
+  MIC.append(0.0625)
+
+plt.step(day, conc, '-', color='black')
+plt.title("E. coli MB1860 to Ertapenem", fontdict={'fontsize':15})
+plt.xticks(fontsize=15)
+plt.xlabel("Elapse time (day)", fontdict={'fontsize':15})
+plt.yscale("linear")
+plt.yticks(fontsize=15)
+plt.ylabel("ETP conc. (mg/L)", fontdict={'fontsize':15})
+
+plt.step(day, MIC, '-', color='red')
+
+plt.figlegend(["Adaptation","MIC"], loc="upper left", bbox_to_anchor=(0.15, 0.9), fontsize=12)
+plt.show()
