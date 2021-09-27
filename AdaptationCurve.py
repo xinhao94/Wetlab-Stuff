6527fbe0ec65fd05d3248383e7d167e381fe94dc
Xinhao Song <songxinhao@hotmail.com>
2021-09-27 17:19:56 -0500

    Update AdaptationCurve.py

--- AdaptationCurve.py
+++ AdaptationCurve.py
@@ -5,8 +5,12 @@ import matplotlib.pyplot as plt
 day = np.arange(0,8)
 conc = [0,0.03,0.0625,0.09,0.18,0.38,1,4]
 MIC = []
+CLSI = []
 for i in range(len(conc)):
   MIC.append(0.0625)
+
+for i in range(len(conc)):
+  CLSI.append(2)
 
 plt.step(day, conc, '-', color='black')
 plt.title("E. coli MB1860 to Ertapenem", fontdict={'fontsize':15})
@@ -17,6 +21,7 @@ plt.yticks(fontsize=15)
 plt.ylabel("ETP conc. (mg/L)", fontdict={'fontsize':15})
 
 plt.step(day, MIC, '-', color='red')
+plt.step(day, CLSI, '-', color='green')
 
-plt.figlegend(["Adaptation","MIC"], loc="upper left", bbox_to_anchor=(0.15, 0.9), fontsize=12)
+plt.figlegend(["Adaptation","MIC","CLSI BP"], loc="upper left", bbox_to_anchor=(0.15, 0.9), fontsize=12)
 plt.show()
