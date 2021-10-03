15d935ea4e271019c55a6e62585fded5968e8732
Xinhao Song <songxinhao@hotmail.com>
2021-10-03 16:00:56 -0500

    Update AdaptationCurve.py

--- AdaptationCurve.py
+++ AdaptationCurve.py
@@ -2,17 +2,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-day = np.arange(0,8)
-conc = [0,0.03,0.0625,0.09,0.18,0.38,1,4]
+day = np.arange(0,13)
+pop1 = [0,0.03,0.0625,0.09,0.18,0.38,1,4,8,16,16,16,32]
+pop2 = [0,0.03,0.0625,0.09,0.18,0.38,1,4,8,16,32,32,32]
+pop3 = [0,0.03,0.0625,0.09,0.18,0.38,1,4,8,32,32,32,32]
+pop4 = [0,0.03,0.0625,0.09,0.18,0.38,1,4,16,32,32,32,32]
+pop5 = [0,0.03,0.0625,0.09,0.18,0.38,1,4,8,16,16,24,32]
+pops = [pop1, pop2, pop3, pop4, pop5]
 MIC = []
 CLSI = []
-for i in range(len(conc)):
+for i in range(len(pop1)):
   MIC.append(0.0625)
 
-for i in range(len(conc)):
+for i in range(len(pop1)):
   CLSI.append(2)
 
-plt.step(day, conc, '-', color='black')
+for pop in pops:
+  plt.step(day, pop, '-', alpha=1)
+
 plt.title("E. coli MB1860 to Ertapenem", fontdict={'fontsize':15})
 plt.xticks(fontsize=15)
 plt.xlabel("Elapse time (day)", fontdict={'fontsize':15})
@@ -20,8 +27,9 @@ plt.yscale("linear")
 plt.yticks(fontsize=15)
 plt.ylabel("ETP conc. (mg/L)", fontdict={'fontsize':15})
 
-plt.step(day, MIC, '-', color='red')
-plt.step(day, CLSI, '-', color='green')
+plt.step(day, MIC, '--', color='black')
+plt.step(day, CLSI, '-.', color='black')
 
-plt.figlegend(["Adaptation","MIC","CLSI BP"], loc="upper left", bbox_to_anchor=(0.15, 0.9), fontsize=12)
+plt.figlegend(["Pop 1", "Pop 2", "Pop 3", "Pop 4", "Pop 5", "MIC","CLSI BP"], 
+              loc="upper left", bbox_to_anchor=(0.12, 0.92), fontsize=12)
 plt.show()
